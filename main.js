08df00822c21beaa97ca693df1bd892161dbb5d0
Meenaprakash21 <110009959+Meenaprakash21@users.noreply.github.com>
2022-12-17 16:48:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function check(){
     var userRemember = document.getElementById("rememberMe");
 
     if(userName.value == storedName && userPw.value == storedPw){
-        window.location.href("puzzle.html");
+        window.location.href="puzzle.html";
         alert('You are logged in.');
     }else{
         alert('Error on login');
